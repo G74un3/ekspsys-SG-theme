e5650f43799613f39c91659659100b5693e150de
G74un3 <daniel@graungaard.com>
2015-05-26 08:18:02 +0200

    Menu's are now shown corrrectly.

--- index.php
+++ index.php
@@ -1,192 +1,137 @@
 <?php get_header(); ?>
 	<div id="main">
-		<div id="content">
-			<h1>Main Area</h1>
+	<div id="content">
+	<h1>Main Area</h1>
 
-			<div id="container">
+	<div id="container">
 
-			<div class="fag-container">
-				<div id="rod1" class="fag">
-					<div id="" class="fagplaceholder"></div>
-					<p> Rod 1</p>
-				</div>
+		<div class="fag-container">
+			<div id="rod1" class="rod">
+				<div id="" class="fagplaceholder"></div>
+				<p> Rod 1</p>
 			</div>
+		</div>
 
-			<div class="fag-container">
-				<div id="rod2" class="fag">
-					<div id="" class="fagplaceholder"></div>
-					<p> Rod 2</p>
-				</div>
+		<div class="fag-container">
+			<div id="rod2" class="rod">
+				<div id="" class="fagplaceholder"></div>
+				<p> Rod 2</p>
 			</div>
+		</div>
 
 
-			<div id="last-child"class="fag-container">
-				<div id="rod3" class="fag">
-					<div id="" class="fagplaceholder"></div>
-					<p> Rod 3</p>
-				</div>
+		<div id="last-child" class="fag-container">
+			<div id="rod3" class="rod">
+				<div id="" class="fagplaceholder"></div>
+				<p> Rod 3</p>
 			</div>
-			<span class="stretch"></span>
-			</div>
-			<div style="clear:both;"></div>
+		</div>
+		<span class="stretch"></span>
+	</div>
+	<div style="clear:both;"></div>
+
+
+	<div style="position: fixed; top: 0px;">
+		<button onclick="placeAndConnect('fag1', 'rod1')">Place fag 1 at rod 1</button>
+		<button onclick="placeAndConnect('fag1', 'rod2')">Place fag 1 at rod 2</button>
+		<button onclick="placeAndConnect('fag1', 'rod3')">Place fag 1 at rod 3</button>
+		<button onclick="placeAndConnect('fag2', 'rod1')">Place fag 2 at rod 1</button>
+		<button onclick="placeAndConnect('fag2', 'rod2')">Place fag 2 at rod 2</button>
+		<button onclick="placeAndConnect('fag2', 'rod3')">Place fag 2 at rod 3</button>
+		<button onclick="toogleVisible('fag1')">toogleVisible fag 1</button>
+		<button onclick="toogleVisible('fag2')">toogleVisible fag 2</button>
+		<button onclick="$('#fag1-container').visibilityToggle()">Hide fag 1</button>
+		<button onclick="$('#fag2-container').visibilityToggle()">Hide fag 2</button>
+		<button onclick="disconnectFag('fag2'); $('#fag2-container').visibilityToggle()">Hide fag 2 REAL</button>
+		<button onclick="placeFag('fag2', 'rod1')">PLACE fag 2 at 1 REAL</button>
+		<button onclick="connectFag('fag2'); $('#fag2-container').visibilityToggle()">Show fag 2 REAL</button>
+		<button onclick="">DOOM!</button>
+	</div>
 
 
 
-			<div style="position: fixed; top: 0px;">
-			<button onclick="placeAndConnect('fag1', 'rod1')">Place fag 1 at rod 1</button>
-			<button onclick="placeAndConnect('fag1', 'rod2')">Place fag 1 at rod 2</button>
-			<button onclick="placeAndConnect('fag1', 'rod3')">Place fag 1 at rod 3</button>
-			<button onclick="placeAndConnect('fag2', 'rod1')">Place fag 2 at rod 1</button>
-			<button onclick="placeAndConnect('fag2', 'rod2')">Place fag 2 at rod 2</button>
-			<button onclick="placeAndConnect('fag2', 'rod3')">Place fag 2 at rod 3</button>
-			<button onclick="toogleVisible('fag1')">toogleVisible fag 1</button>
-			<button onclick="toogleVisible('fag2')">toogleVisible fag 2</button>
-			<button onclick="$('#fag1-container').visibilityToggle()">Hide fag 1</button>
-			<button onclick="$('#fag2-container').visibilityToggle()">Hide fag 2</button>
-			<button onclick="disconnectFag('fag2'); $('#fag2-container').visibilityToggle()">Hide fag 2 REAL</button>
-			<button onclick="placeFag('fag2', 'rod1')">PLACE fag 2 at 1 REAL</button>
-			<button onclick="connectFag('fag2'); $('#fag2-container').visibilityToggle()">Show fag 2 REAL</button>
-			<button onclick="test()">DOOM!</button>
-			</div>
+	<?php
+	//Where all of the nodes and leaves are generated
+	include( get_template_directory() . '/the_loop.php' );
+	include( get_template_directory() . '/jquery_functions.php' );?>
+
+
+<style>
+
+	.greyed {
+
+		color: #90C !important;
+
+	}
+
+	#fagmenu a {
+
+		margin: 10px;
+
+	}
+
+</style>
+
+<?php printFagMenu($fag); ?>
+
+
+	<script type="text/JavaScript">
 
 
 
-			<?php
-			//Where all of the nodes and leaves are generated
-			include( get_template_directory() . '/the_loop.php' );
-			include( get_template_directory() . '/jquery_functions.php' );?>
+		$(".rod").each(function () {
+
+			$(this).qtip({
+				prerender: true,
+				hide:{ //makes the qtip stay so it is clickable,
+					delay: 500, //give a small delay to allow the user to mouse over it.
+					fixed: true
+				},
+				content: {
+					title: 'Vælg et fag!',
+					text: $('#fagmenu').clone().get()
+				},
+				position: {
+					my: "top center",
+					at: "bottom center"
+				}, style: { classes: 'fagmenuboks ' + $(this).attr('id') }
+			});
+
+		});
 
 
+		$('.side').each(function () { //Attatches thumbnail tips to nodes with thumbnail
 
-			<script type="text/JavaScript">
+			var image = $(this).attr('qtip-attr'); //Get the thumbnail url
 
+			if (image.length > 0) {
 
-				$(".fag").qtip({
+				$(this).qtip({
 					content: {
-						title: 'I like turtles!',
-						text: 'I\'m at the top right of my target'
+						title: $(this).children('p').eq(0).text(),
+						text: '<img src="' + image + '" width="100">'
 					},
 					position: {
 						my: "top center",
 						at: "bottom center"
-					}
+					},
+					style: {
+						classes: 'tip-billede'}
 				});
 
-
-				function test() {
-
-				}
+			}
 
 
-				function toogleVisible(fag) {
-
-					var fagid = '#' + fag;
-					var fagcontainerid = fagid + "-container";
-
-					if ($(fagcontainerid).is(":visible")) { //Visible
-
-						//$(fagcontainerid).css('visibility', 'hidden');
-						//$(fagcontainerid).hide();
-						$(fagcontainerid).css("display", "none")
-
-					} else { //Invisible
-
-						//$(fagcontainerid).css('visibility', 'visible');
-						//$(fagcontainerid).show();
-						$(fagcontainerid).css("display", "block")
-					}
-
-				}
-
-				jQuery.fn.visible = function() {
-					return this.css('visibility', 'visible');
-				};
-
-				jQuery.fn.invisible = function() {
-					return this.css('visibility', 'hidden');
-				};
-
-				jQuery.fn.visibilityToggle = function() {
-					return this.css('visibility', function(i, visibility) {
-						return (visibility == 'visible') ? 'hidden' : 'visible';
-					});
-				};
-
-				function placeFag(fag, rod) {
-
-					var fagid = idfy(fag);
-
-					$(fagid).connections('remove'); //Removes any previous connections making shure that the treee will only be connected to the new root
-					$(fagid).removeAttr('id'); //removes fag id from any root that cuurently uses is thereby making shure that the tree is moved correctly
-					$('#' + rod + ' .fagplaceholder').attr("id", fag);
+		});
 
 
-					var functioncall = function(element) {
+		<?php// generateConnectionFunctions($array_of_placements_by_parent)?>
 
-						place(element.attr('id'), element.attr('parent'))
-
-					};
-
-					changeTree(functioncall, fag);
-
-					//run = convertToFunction("place" + fag);
-					//run();
-
-				}
+	</script>
+	<?php // print_r($array_of_placements_by_parent)?>
 
 
-				function connectFag(fag) {
-
-
-					//disconnectFag(fag);
-
-					var functioncall = function(element) {
-
-						connect(element.attr('id'), element.attr('parent'));
-
-					};
-
-					changeTree(functioncall, fag);
-
-				}
-
-				function placeAndConnect(fag, rod){
-
-					placeFag(fag, rod);
-					connectFag(fag);
-
-
-				}
-
-
-
-
-				function disconnectFag(fag) {
-
-
-
-				}
-
-
-				function convertToFunction(string){
-
-					var functionnamestring = string; //Generates name of  function
-					var functiontocall = window[functionnamestring]; //Gets reference to the function from window scope
-					if (typeof functiontocall === "function") return functiontocall; //Checks to make shure we have really retrieved a function before running it
-
-				}
-
-
-
-
-				<?php// generateConnectionFunctions($array_of_placements_by_parent)?>
-
-			</script>
-			<?php// print_r($array_of_placements_by_parent)?>
-
-
-
-		</div>
+	</div>
 	</div>
 	<div id="delimiter">
 	</div>
--- js/drawtrees.js
+++ js/drawtrees.js
@@ -9,7 +9,7 @@ function changeTree(functioncall, fag) {
     elements = $("." + fag + '.row' + i);
 
 
-    while(elements.length > 0) {
+    while (elements.length > 0) {
 
         elements.each(function () {
 
@@ -48,7 +48,10 @@ function place(nodeid, parentid) {
     var x_offset = parseFloat(x_string);
     var y_offset = parseFloat(y_string);
 
-    $(nodeid).offset({top: parent_pos.top - y_offset , left: parent_pos.left + (parent_width/2) - (element_width/2) + x_offset});
+    $(nodeid).offset({
+        top: parent_pos.top - y_offset,
+        left: parent_pos.left + (parent_width / 2) - (element_width / 2) + x_offset
+    });
 
 }
 
@@ -59,8 +62,7 @@ function connect(nodeid, parentid) {
     parentid = idfy(parentid);
 
 
-    $(parentid).connections({to: nodeid , 'class': 'line'});
-
+    $(parentid).connections({to: nodeid, 'class': 'line'});
 
 
 }
@@ -75,6 +77,218 @@ function idfy(id) {
 function classyfy(aclass) {
 
     return '.' + aclass;
+
+}
+
+
+function connectFag(fag) {
+
+
+    var functioncall = function (element) {
+
+        connect(element.attr('id'), element.attr('parent'));
+
+    };
+
+    changeTree(functioncall, fag);
+
+}
+
+function placeAndConnect(fag, rod) {
+
+
+    disconnectFag(fag);
+    placeFag(fag, rod);
+    connectFag(fag);
+
+
+
+}
+
+
+function menuItemClicked(fag, rod) {
+
+    var fagclass  = classyfy(fag);
+    var atrname = 'lastrod';
+
+
+    var lastrod = $(fagclass).attr(atrname);
+
+
+    var currentFag = $(idfy(rod) + ' .fagplaceholder').attr('id');
+
+
+    if(currentFag == fag || currentFag == "") {
+
+        if (lastrod == undefined) { //element is not placed
+
+            $(fagclass).attr(atrname, rod);
+
+            toogleVisible(fag);
+            updateItemGrey(fag);
+
+            placeAndConnect(fag, rod);
+
+        } else if (lastrod == rod) { //element should be hidden
+
+            $(fagclass).removeAttr(atrname);
+
+            toogleVisible(fag);
+            updateItemGrey(fag);
+
+        } else { //Rote should be moved
+
+            $(fagclass).attr(atrname, rod);
+
+            placeAndConnect(fag, rod);
+
+
+        }
+
+    }
+
+
+
+
+}
+
+
+function updateItemGrey(fag) {
+
+    var menuitem = $('#fagmenu ' + classyfy(fag));
+    var menuitemclasses = menuitem.attr('class').split(' '); //Gets an array of all classes from the menuitem
+
+    //Toggles the state of greyed
+    if(getSpecficClassFromArray('greyed', menuitemclasses) == "") {
+
+        menuitem.addClass('greyed');
+
+    } else {
+
+        menuitem.removeClass('greyed');
+
+    }
+
+
+}
+
+
+function disconnectFag(fag) {
+
+    fag = classyfy(fag);
+
+    $(fag).each(function () {
+
+        id = $(this).attr('id');
+
+        disconnect(id);
+
+    });
+
+
+}
+
+
+function disconnect(nodeid) {
+
+
+    nodeid = idfy(nodeid);
+
+    $(nodeid).connections('remove');
+
+}
+
+function placeFag(fag, rod) {
+
+
+
+
+    var fagid = idfy(fag);
+
+    $(fagid).connections('remove'); //Removes any previous connections making shure that the treee will only be connected to the new root
+    $(fagid).removeAttr('id'); //removes fag id from any root that cuurently uses is thereby making shure that the tree is moved correctly
+    $('#' + rod + ' .fagplaceholder').attr('id', fag);
+
+
+    var functioncall = function (element) {
+
+        place(element.attr('id'), element.attr('parent'))
+
+    };
+
+    changeTree(functioncall, fag);
+
+
+
+}
+
+
+function toogleVisible(fag) {
+
+    var fagid = '#' + fag;
+    var fagcontainerid = fagid + "-container";
+
+    if ($(fagcontainerid).is(":visible")) { //Visible
+
+        //$(fagcontainerid).css('visibility', 'hidden');
+        //$(fagcontainerid).hide();
+        $(fagcontainerid).css("display", "none")
+
+    } else { //Invisible
+
+        //$(fagcontainerid).css('visibility', 'visible');
+        //$(fagcontainerid).show();
+        $(fagcontainerid).css("display", "block")
+    }
+
+}
+
+jQuery.fn.visible = function () {
+    return this.css('visibility', 'visible');
+};
+
+jQuery.fn.invisible = function () {
+    return this.css('visibility', 'hidden');
+};
+
+jQuery.fn.visibilityToggle = function () {
+    return this.css('visibility', function (i, visibility) {
+        return (visibility == 'visible') ? 'hidden' : 'visible';
+    });
+};
+
+
+function getRodID(element) {
+
+
+    var classes = element.parent().parent().parent().attr('class');
+
+    var arrayofclasses = classes.split(' ');
+
+    var rodid = getSpecficClassFromArray('rod', arrayofclasses);
+
+
+    return rodid;
+
+
+}
+
+
+function getSpecficClassFromArray(string, array) {
+
+
+    for (var index in array) {
+
+        if (!array[index].search(string)) {
+
+            return array[index];
+
+        }
+
+
+    }
+
+    return "";
 
 }
 
